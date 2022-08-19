4cf413a3df275fc04611b69ea0c0701950c36f93
Md Tohidul Islam <tohidul.islam@dsinnovators.com>
2022-08-19 15:17:22 +0600

    get data type added

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ def getDiemnsion(npArray: np.ndarray) -> int:
 def getShape(npArray: np.ndarray) -> tuple:
     return npArray.shape
 
+def getDtataType(array: np.ndarray):
+    return array.dtype
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     a = [[1,2,3], [45,3,1]]
     b = [1,2,3,43,5345]
     npArray = getNumpyArray(a)
+    npArray2 = getNumpyArray(a, 'int8')
+    npArray3 = getNumpyArray(a, 'int16')
     # print(getNumpyArray(a))
-    print(getDiemnsion(npArray))
-    print(getShape(getNumpyArray(b)))
-    print(type(npArray))
+    print(getDtataType(npArray2))
+    print(getDtataType(npArray3))
+    print(getDtataType(npArray))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
